a8485d38a875a320836eb9ec5c4073ff5fb5763b
Anton Apraksin <anton.apraksin@teamdev.com>
2022-01-21 15:22:03 +0200

    change: StepEvent and StepParameter implemented. Context changed

---
+++ src/interactor/state_machine/StepEvent.h
@@ -0,0 +1,19 @@
+#ifndef TASKMANAGER_SRC_INTERACTOR_STATE_MACHINE_STEPEVENT_H_
+#define TASKMANAGER_SRC_INTERACTOR_STATE_MACHINE_STEPEVENT_H_
+
+namespace task_manager {
+enum class StepEvent {
+  kNothing,
+  kNotPresentId,
+  kLoadFailure,
+  kSaveFailure,
+  kNotPresentLabel,
+
+  kShowByLabel,
+  kShowAll,
+  kShowById,
+  kShowId
+};
+}
+
+#endif  // TASKMANAGER_SRC_INTERACTOR_STATE_MACHINE_STEPEVENT_H_
---
+++ src/interactor/state_machine/interactor_steps/StepParameter.h
@@ -0,0 +1,13 @@
+#ifndef TASKMANAGER_SRC_INTERACTOR_STATE_MACHINE_INTERACTOR_STEPS_STEPPARAMETER_H_
+#define TASKMANAGER_SRC_INTERACTOR_STATE_MACHINE_INTERACTOR_STEPS_STEPPARAMETER_H_
+
+#include "interactor/state_machine/Context.h"
+
+namespace task_manager {
+struct StepParameter {
+  task_manager::Context ctx;
+  SolidTasks cache;
+};
+}  // namespace task_manager
+
+#endif  // TASKMANAGER_SRC_INTERACTOR_STATE_MACHINE_INTERACTOR_STEPS_STEPPARAMETER_H_

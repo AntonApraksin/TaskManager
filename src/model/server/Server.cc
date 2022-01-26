1118525a9d641182ceee444dd16f54d49772f6a8
Anton Apraksin <anton.apraksin@teamdev.com>
2022-01-26 15:48:58 +0200

    feat: first draft of server

---
+++ src/model/server/Server.cc
@@ -0,0 +1,110 @@
+#include "Server.h"
+
+#include "model/ModelController.h"
+
+namespace task_manager {
+
+ModelControllerStatus ModelControllerStatusToProtoStatus(
+    ModelController::Status status)
+{
+  switch (status) {
+    case ModelController::Status::kNotPresentId:
+      return ModelControllerStatus::kNotPresentId;
+    case ModelController::Status::kOk:
+      return ModelControllerStatus::kOk;
+    case ModelController::Status::kLoadFailure:
+      return ModelControllerStatus::kLoadFailure;
+    case ModelController::Status::kSaveFailure:
+      return ModelControllerStatus::kSaveFailure;
+  }
+}
+
+
+Server::Server(std::unique_ptr<ModelController> model_controller)
+    : model_controller_(std::move(model_controller)) {}
+
+grpc::Status Server::AddTask(::grpc::ServerContext *context,
+                             const ::task_manager::Task *request,
+                             ::task_manager::TaskIdResponse *response) {
+  auto result = model_controller_->Add(*request);
+  if (result)
+  {
+    response->set_allocated_task_id(new TaskId(result.AccessResult()));
+  }
+  response->set_status(ModelControllerStatusToProtoStatus(result.GetStatus()));
+  return grpc::Status::OK;
+}
+
+grpc::Status Server::AddSubtask(
+    ::grpc::ServerContext *context,
+    const ::task_manager::TaskAndTaskIdRequest *request,
+    ::task_manager::TaskIdResponse *response) {
+  auto result = model_controller_->Add(request->task_id(), request->task());
+  if (result)
+  {
+    response->set_allocated_task_id(new TaskId(result.AccessResult()));
+  }
+  response->set_status(ModelControllerStatusToProtoStatus(result.GetStatus()));
+  return grpc::Status::OK;
+}
+
+grpc::Status Server::Edit(::grpc::ServerContext *context,
+                          const ::task_manager::TaskAndTaskIdRequest *request,
+                          ::task_manager::PlainResponse *response) {
+  auto result = model_controller_->Edit(request->task_id(), request->task());
+  response->set_status(ModelControllerStatusToProtoStatus(result.GetStatus()));
+  return grpc::Status::OK;
+}
+
+grpc::Status Server::Complete(::grpc::ServerContext *context,
+                              const ::task_manager::TaskId *request,
+                              ::task_manager::PlainResponse *response) {
+  auto result = model_controller_->Complete(*request);
+  response->set_status(ModelControllerStatusToProtoStatus(result.GetStatus()));
+  return grpc::Status::OK;
+}
+
+grpc::Status Server::Delete(::grpc::ServerContext *context,
+                            const ::task_manager::TaskId *request,
+                            ::task_manager::PlainResponse *response) {
+  auto result = model_controller_->Delete(*request);
+  response->set_status(ModelControllerStatusToProtoStatus(result.GetStatus()));
+  return grpc::Status::OK;
+}
+
+grpc::Status Server::GetAllSolidTasks(
+    ::grpc::ServerContext *context, const ::google::protobuf::Empty *request,
+    ::task_manager::SolidTasksResponse *response) {
+  auto result = model_controller_->GetAllSolidTasks();
+  for (const auto &i : result.AccessResult()) {
+    response->add_solid_tasks()->CopyFrom(i);
+  }
+  response->set_status(ModelControllerStatusToProtoStatus(result.GetStatus()));
+  return grpc::Status::OK;
+}
+
+grpc::Status Server::GetSpecifiedSolidTasks(
+    ::grpc::ServerContext *context, const TaskIdsRequest *request,
+    ::task_manager::SolidTasksResponse *response) {
+  std::vector<TaskId> ids;
+  std::copy(request->task_ids().cbegin(), request->task_ids().cend(), std::back_inserter(ids));
+  auto result = model_controller_->GetSpecificSolidTasks(std::move(ids));
+  for (const auto &i : result.AccessResult()) {
+    response->add_solid_tasks()->CopyFrom(i);
+  }
+  response->set_status(ModelControllerStatusToProtoStatus(result.GetStatus()));
+  return grpc::Status::OK;
+}
+
+grpc::Status Server::Load(::grpc::ServerContext *context,
+                          const ::google::protobuf::Empty *request,
+                          ::task_manager::PlainResponse *response) {
+  return Service::Load(context, request, response);
+}
+
+grpc::Status Server::Save(::grpc::ServerContext *context,
+                          const ::google::protobuf::Empty *request,
+                          ::task_manager::PlainResponse *response) {
+  return Service::Save(context, request, response);
+}
+}
---
+++ src/model/server/Server.h
@@ -0,0 +1,60 @@
+#ifndef TASKMANAGER_SRC_MODEL_SERVER_SERVER_H_
+#define TASKMANAGER_SRC_MODEL_SERVER_SERVER_H_
+
+#include <grpcpp/grpcpp.h>
+
+#include <memory>
+
+#include "ModelService.grpc.pb.h"
+
+namespace task_manager
+{
+class ModelController;
+
+class Server final : public task_manager::ModelService::Service {
+ public:
+  Server(std::unique_ptr<ModelController> model_controller);;
+
+  grpc::Status AddTask(::grpc::ServerContext *context,
+                       const ::task_manager::Task *request,
+                       ::task_manager::TaskIdResponse *response) override;
+
+  grpc::Status AddSubtask(::grpc::ServerContext *context,
+                          const ::task_manager::TaskAndTaskIdRequest *request,
+                          ::task_manager::TaskIdResponse *response) override;
+
+  grpc::Status Edit(::grpc::ServerContext *context,
+                    const ::task_manager::TaskAndTaskIdRequest *request,
+                    ::task_manager::PlainResponse *response) override;
+
+  grpc::Status Complete(::grpc::ServerContext *context,
+                        const ::task_manager::TaskId *request,
+                        ::task_manager::PlainResponse *response) override;
+
+  grpc::Status Delete(::grpc::ServerContext *context,
+                      const ::task_manager::TaskId *request,
+                      ::task_manager::PlainResponse *response) override;
+
+  grpc::Status GetAllSolidTasks(
+      ::grpc::ServerContext *context, const ::google::protobuf::Empty *request,
+      ::task_manager::SolidTasksResponse *response) override;
+
+  grpc::Status GetSpecifiedSolidTasks(
+      ::grpc::ServerContext *context, const TaskIdsRequest *request,
+      ::task_manager::SolidTasksResponse *response) override;
+
+  grpc::Status Load(::grpc::ServerContext *context,
+                    const ::google::protobuf::Empty *request,
+                    ::task_manager::PlainResponse *response) override;
+
+  grpc::Status Save(::grpc::ServerContext *context,
+                    const ::google::protobuf::Empty *request,
+                    ::task_manager::PlainResponse *response) override;
+
+ private:
+  std::unique_ptr<ModelController> model_controller_;
+};
+
+}
+
+#endif  // TASKMANAGER_SRC_MODEL_SERVER_SERVER_H_

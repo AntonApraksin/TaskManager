fcf42b6af82326c99b9161092bd905308c30ffa2
Anton Apraksin <anton.apraksin@teamdev.com>
2022-01-27 18:16:18 +0200

    feat: gRPC client implemented

---
+++ src/client/TaskServiceModelController.h
@@ -0,0 +1,33 @@
+#ifndef TASKMANAGER_SRC_CLIENT_TASKSERVICEMODELCONTROLLER_H_
+#define TASKMANAGER_SRC_CLIENT_TASKSERVICEMODELCONTROLLER_H_
+
+#include <grpcpp/grpcpp.h>
+
+#include <memory>
+
+#include "TaskService.grpc.pb.h"
+#include "model/ModelController.h"
+
+namespace task_manager {
+class TaskServiceModelController : public ModelController {
+ public:
+  TaskServiceModelController(
+      std::unique_ptr<task_manager::TaskService::Stub> stub);
+
+  OperationResult<Status, TaskId> Add(Task task) override;
+  OperationResult<Status, TaskId> Add(TaskId task_id, Task task) override;
+  OperationResult<Status> Edit(TaskId id, Task task) override;
+  OperationResult<Status> Complete(TaskId id) override;
+  OperationResult<Status> Delete(TaskId id) override;
+  OperationResult<Status, SolidTasks> GetAllSolidTasks() override;
+  OperationResult<Status, SolidTasks> GetSpecificSolidTasks(
+      std::vector<TaskId> vector) override;
+  OperationResult<Status> Load() override;
+  OperationResult<Status> Save() override;
+
+ private:
+  std::unique_ptr<task_manager::TaskService::Stub> stub_;
+};
+}  // namespace task_manager
+
+#endif  // TASKMANAGER_SRC_CLIENT_TASKSERVICEMODELCONTROLLER_H_

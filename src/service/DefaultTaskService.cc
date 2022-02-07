b8a4328ac672991002764fbaa51259e8cc7008e0
Anton Apraksin <anton.apraksin@teamdev.com>
2022-02-07 14:19:54 +0200

    fix: reformat code

--- src/service/DefaultTaskService.cc
+++ src/service/DefaultTaskService.cc
@@ -14,11 +14,11 @@ namespace task_manager {
 TaskServiceStatus ConvertModelControllerStatusToTaskServiceStatus(
     ModelController::Status status) {
     switch (status) {
-        case ModelController::Status::kNotPresentId:return TaskServiceStatus::kNotPresentId;
-        case ModelController::Status::kOk:return TaskServiceStatus::kOk;
-        case ModelController::Status::kLoadFailure:return TaskServiceStatus::kLoadFailure;
-        case ModelController::Status::kSaveFailure:return TaskServiceStatus::kSaveFailure;
-        case ModelController::Status::kNotPresentLabel:return TaskServiceStatus::kNotPresentLabel;
+        case ModelController::Status::kNotPresentId: { return TaskServiceStatus::kNotPresentId; }
+        case ModelController::Status::kOk: { return TaskServiceStatus::kOk; }
+        case ModelController::Status::kLoadFailure: { return TaskServiceStatus::kLoadFailure; }
+        case ModelController::Status::kSaveFailure: { return TaskServiceStatus::kSaveFailure; }
+        case ModelController::Status::kNotPresentLabel: { return TaskServiceStatus::kNotPresentLabel; }
     }
 }
 
@@ -26,25 +26,25 @@ DefaultTaskService::DefaultTaskService(
     std::unique_ptr<ModelController> model_controller)
     : model_controller_(std::move(model_controller)) {}
 
-grpc::Status DefaultTaskService::AddTask(
-    ::grpc::ServerContext *, const ::task_manager::Task *request,
-    ::task_manager::TaskIdResponse *response) {
-  BOOST_LOG_NAMED_SCOPE("DefaultTaskService::AddTask");
-  auto &logger = logging::GetDefaultLogger();
+grpc::Status DefaultTaskService::AddTask(::grpc::ServerContext *,
+                                         const ::task_manager::Task *request,
+                                         ::task_manager::TaskIdResponse *response) {
+    BOOST_LOG_NAMED_SCOPE("DefaultTaskService::AddTask");
+    auto &logger = logging::GetDefaultLogger();
 
-  BOOST_LOG_SEV(logger, logging::severinity::debug)
-      << "request: " << request->DebugString();
+    BOOST_LOG_SEV(logger, logging::severinity::debug)
+        << "request: " << request->DebugString();
 
-  auto result = model_controller_->Add(*request);
-  if (result) {
-    response->set_allocated_task_id(new TaskId(result.AccessResult()));
-  }
-  response->set_status(
-      ConvertModelControllerStatusToTaskServiceStatus(result.GetStatus()));
+    auto result = model_controller_->Add(*request);
+    if (result) {
+        response->set_allocated_task_id(new TaskId(result.AccessResult()));
+    }
+    response->set_status(
+        ConvertModelControllerStatusToTaskServiceStatus(result.GetStatus()));
 
-  BOOST_LOG_SEV(logger, logging::severinity::debug) << response->DebugString();
+    BOOST_LOG_SEV(logger, logging::severinity::debug) << response->DebugString();
 
-  return grpc::Status::OK;
+    return grpc::Status::OK;
 }
 
 grpc::Status DefaultTaskService::AddSubtask(::grpc::ServerContext *,
@@ -86,44 +86,44 @@ grpc::Status DefaultTaskService::Delete(::grpc::ServerContext *,
     return grpc::Status::OK;
 }
 
-grpc::Status DefaultTaskService::GetAllSolidTasks(
-    ::grpc::ServerContext *, const ::google::protobuf::Empty *,
-    ::task_manager::SolidTasksResponse *response) {
-  auto result = model_controller_->GetAllSolidTasks();
-  if (result) {
-    for (const auto &i : result.AccessResult()) {
-      response->add_solid_tasks()->CopyFrom(i);
+grpc::Status DefaultTaskService::GetAllSolidTasks(::grpc::ServerContext *,
+                                                  const ::google::protobuf::Empty *,
+                                                  ::task_manager::SolidTasksResponse *response) {
+    auto result = model_controller_->GetAllSolidTasks();
+    if (result) {
+        for (const auto &i: result.AccessResult()) {
+            response->add_solid_tasks()->CopyFrom(i);
+        }
     }
-  }
     response->set_status(
-    ConvertModelControllerStatusToTaskServiceStatus(result.GetStatus()));
+        ConvertModelControllerStatusToTaskServiceStatus(result.GetStatus()));
     return grpc::Status::OK;
 }
 
-grpc::Status DefaultTaskService::GetSpecifiedSolidTasks(
-    ::grpc::ServerContext *, const TaskIdsRequest *request,
-    ::task_manager::SolidTasksResponse *response) {
-  BOOST_LOG_NAMED_SCOPE("DefaultTaskService::GetSpecifiedSolidTasks");
-  auto &logger = logging::GetDefaultLogger();
+grpc::Status DefaultTaskService::GetSpecifiedSolidTasks(::grpc::ServerContext *,
+                                                        const TaskIdsRequest *request,
+                                                        ::task_manager::SolidTasksResponse *response) {
+    BOOST_LOG_NAMED_SCOPE("DefaultTaskService::GetSpecifiedSolidTasks");
+    auto &logger = logging::GetDefaultLogger();
 
-  BOOST_LOG_SEV(logger, logging::severinity::debug)
-      << "request: " << request->DebugString();
+    BOOST_LOG_SEV(logger, logging::severinity::debug)
+        << "request: " << request->DebugString();
 
-  std::vector<TaskId> ids;
-  std::copy(request->task_ids().cbegin(), request->task_ids().cend(),
-            std::back_inserter(ids));
-  auto result = model_controller_->GetSpecificSolidTasks(std::move(ids));
-  if (result) {
-    for (const auto &i : result.AccessResult()) {
-      response->add_solid_tasks()->CopyFrom(i);
+    std::vector<TaskId> ids;
+    std::copy(request->task_ids().cbegin(), request->task_ids().cend(),
+              std::back_inserter(ids));
+    auto result = model_controller_->GetSpecificSolidTasks(std::move(ids));
+    if (result) {
+        for (const auto &i: result.AccessResult()) {
+            response->add_solid_tasks()->CopyFrom(i);
+        }
     }
-  }
-  response->set_status(
-      ConvertModelControllerStatusToTaskServiceStatus(result.GetStatus()));
+    response->set_status(
+        ConvertModelControllerStatusToTaskServiceStatus(result.GetStatus()));
 
-  BOOST_LOG_SEV(logger, logging::severinity::debug)
-      << "response: " << TaskServiceStatus_Name(response->status());
-  return grpc::Status::OK;
+    BOOST_LOG_SEV(logger, logging::severinity::debug)
+        << "response: " << TaskServiceStatus_Name(response->status());
+    return grpc::Status::OK;
 }
 
 grpc::Status DefaultTaskService::Load(::grpc::ServerContext *,

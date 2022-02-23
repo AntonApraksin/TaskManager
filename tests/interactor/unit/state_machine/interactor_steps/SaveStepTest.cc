4f1e551f5bcc6d5eae5a75f75a395f22f98d7539
Anton Apraksin <anton.apraksin@teamdev.com>
2022-02-23 16:00:39 +0200

    more tests added to Load/SaveStepTest

--- tests/interactor/unit/state_machine/interactor_steps/SaveStepTest.cc
+++ tests/interactor/unit/state_machine/interactor_steps/SaveStepTest.cc
@@ -4,6 +4,8 @@
 #include "test_utils/TaskFactory.h"
 #include "test_utils/utils.h"
 
+#include "interactor/state_machine/interactor_steps/FinalizeStep.h"
+
 class SaveStepTest : public StepTest
 {
 public:
@@ -24,4 +26,18 @@ TEST_F(SaveStepTest, ExecuteWithoutArgumentReturnSaveTasksCommand)
     SetArg("");
     auto command{step_->execute(step_parameter_)};
     EXPECT_NE(dynamic_cast<SaveTasksCommand*>(command.get()), nullptr);
+}
+
+TEST_F(SaveStepTest, ExecuteWithArgumentReturnVoidCommand)
+{
+    SetArg("foo");
+    auto command{step_->execute(step_parameter_)};
+    EXPECT_NE(dynamic_cast<VoidCommand*>(command.get()), nullptr);
+}
+
+TEST_F(SaveStepTest, ChangeStepMustReturnFinalizeStep)
+{
+    SetArg("");
+    auto next_step = step_->ChangeStep();
+    EXPECT_NE(dynamic_cast<FinalizeStep*>(next_step.get()), nullptr);
 }
